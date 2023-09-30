f911add60bd067ae46571e9f2cbd402e186f9b5c
pradeep <724pradeep@gmail.com>
2023-09-30 06:27:17 +0530

    async added

--- src/App.js
+++ src/App.js
@@ -5,22 +5,19 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const fetchMovieHandler = () => {
-    fetch("https://swapi.dev/api/films")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const transformedData = data.results.map((movieData) => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releseDate: movieData.release_date,
-          };
-        });
-        setMovies(transformedData);
-      });
+  const fetchMovieHandler = async () => {
+    const response = await fetch("https://swapi.dev/api/films");
+    const data = await response.json();
+
+    const transformedData = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releseDate: movieData.release_date,
+      };
+    });
+    setMovies(transformedData);
   };
 
   return (
